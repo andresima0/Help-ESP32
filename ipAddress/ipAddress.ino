1c58aea3dce17998c30ae5b710067c435e402b51
Andre Simao <111400782+andresima0@users.noreply.github.com>
2023-07-20 19:03:41 -0300

    Add files via upload

---
+++ ipAddress/ipAddress.ino
@@ -0,0 +1,25 @@
+#include <WiFi.h>
+
+const char* ssid ="xxxxx";     // Replace with your Wi-Fi network name (SSID)
+const char* password = "xxxxx"; // Replace with your Wi-Fi network password
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+}
+
+void loop() {
+  // Check if connected to WiFi
+  if (WiFi.status() == WL_CONNECTED) {
+    // Get and print the local IP address
+    Serial.print("The IP address is: ");
+    Serial.println(WiFi.localIP());
+  }
+  
+  delay(5000); // Wait for 5 seconds
+}
