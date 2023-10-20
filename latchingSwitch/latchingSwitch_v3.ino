765da129461cef5199afd27f1da643fc26bcfd5a
Andre Simao <111400782+andresima0@users.noreply.github.com>
2023-10-20 02:13:38 -0300

    Update latchingSwitch_v3.ino

--- latchingSwitch/latchingSwitch_v3.ino
+++ latchingSwitch/latchingSwitch_v3.ino
@@ -3,93 +3,62 @@ e apresenta o estado atual via requisições Bluetooth, para algum aplicativo de
 
 #include <BLEDevice.h>
 #include <BLEServer.h>
+#include <BLEUtils.h>
 
-const int buttonPin = 12;           // Pino do botão físico
-const int outputPins[] = {25, 26, 27};  // Pinos de saída física
-int currentOutput = 0;              // Índice da saída atual
-int buttonState = 0;                // Estado atual do botão
-int lastButtonState = HIGH;         // Estado anterior do botão
-unsigned long lastDebounceTime = 0; // Tempo do último debounce
-unsigned long debounceDelay = 50;   // Tempo de debounce (evita ruídos)
-bool outputStates[] = {false, false, false}; // Estados das saídas
-unsigned long lastToggleTime = 0;   // Tempo da última troca de saída
-unsigned long toggleDelay = 500;    // Intervalo de troca de saída em milissegundos
+const int buttonPin = 12;
+const int outputPins[] = {25, 26, 27};
+int currentOutput = 0;
+int buttonState = 0;
+int lastButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+bool outputState[] = {false, false, false};
 
-// Definição dos UUIDs para o serviço e característica Bluetooth
-#define SERVICE_UUID        "abcd1234-ab12-cd34-a123-456789abcdef"
-#define CHARACTERISTIC_UUID "abcd1234-ab12-cd34-a123-456789abcdef"
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
 
-// Inicialização do dispositivo BLE
-BLEServer *pServer;
-BLECharacteristic *pCharacteristic;
-
-// Função para alternar as saídas e enviar requisições Bluetooth
-void toggleOutput() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - lastToggleTime >= toggleDelay) {
-    outputStates[currentOutput] = !outputStates[currentOutput];
-    digitalWrite(outputPins[currentOutput], outputStates[currentOutput]);
-
-    // Adicione aqui o código para enviar a requisição Bluetooth correspondente
-    pCharacteristic->setValue(std::to_string(100 * currentOutput + 1));
-    pCharacteristic->notify();
-
-    currentOutput = (currentOutput + 1) % 3; // Avança para a próxima saída em sequência
-
-    lastToggleTime = currentMillis;
-  }
-}
-
-// Classe para manipular as requisições Bluetooth
-class MyCallbacks : public BLECharacteristicCallbacks {
-  void onWrite(BLECharacteristic *pCharacteristic) {
+class CharacteristicCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic* pCharacteristic) {
     std::string value = pCharacteristic->getValue();
-
-    for (int i = 0; i < 3; i++) {
-      if (value == std::to_string(100 * (i+1) + 1)) {
-        toggleOutput(); // Quando um valor é escrito na característica, troca a saída e envia a requisição Bluetooth
-        pCharacteristic->setValue(""); // Limpa o valor da característica após ação
-        pCharacteristic->notify();
-      }
+    String statusMessage = "porta " + String(outputPins[currentOutput]) + " ";
+    
+    if (value == "1") {
+      outputState[currentOutput] = true;
+      digitalWrite(outputPins[currentOutput], HIGH);
+      statusMessage += "ligada = " + String(100 + outputPins[currentOutput]);
+    } else if (value == "0") {
+      outputState[currentOutput] = false;
+      digitalWrite(outputPins[currentOutput], LOW);
+      statusMessage += "desligada = " + String(100 + outputPins[currentOutput]);
     }
+
+    currentOutput = (currentOutput + 1) % 3;
+    pCharacteristic->setValue(statusMessage.c_str());
+    pCharacteristic->notify();
   }
 };
 
-// Configuração inicial do programa
 void setup() {
-  // Configura os pinos como saídas e define o estado inicial como LOW (desligado)
+  BLEDevice::init("ESP32_BLE_Switch");
+  pServer = BLEDevice::createServer();
+  
+  BLEService* pService = pServer->createService(BLEUUID("abcd1234-ab12-cd34-a123-456789abcdef"));
+  pCharacteristic = pService->createCharacteristic(BLEUUID("abcd1234-ab12-cd34-a123-456789abcdef"), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+
+  pCharacteristic->setCallbacks(new CharacteristicCallbacks());
+  pService->start();
+  BLEAdvertising* pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+
   for (int i = 0; i < 3; i++) {
     pinMode(outputPins[i], OUTPUT);
-    digitalWrite(outputPins[i], LOW);
   }
-
-  // Inicializa o dispositivo BLE com um nome
-  BLEDevice::init("ESP32_TESTE");
-
-  // Cria um servidor BLE e um serviço
-  pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  // Cria uma característica BLE com propriedades de leitura e escrita
-  pCharacteristic = pService->createCharacteristic(
-    CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_WRITE
-  );
-
-  // Define a função de callback para a característica BLE
-  pCharacteristic->setCallbacks(new MyCallbacks());
-
-  // Inicia o serviço e a publicidade BLE
-  pService->start();
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
+  pinMode(buttonPin, INPUT_PULLUP);
 }
 
-// Loop principal do programa
 void loop() {
   int reading = digitalRead(buttonPin);
-
+  
   if (reading != lastButtonState) {
     lastDebounceTime = millis();
   }
@@ -99,9 +68,13 @@ void loop() {
       buttonState = reading;
 
       if (buttonState == LOW) {
-        toggleOutput(); // Quando o botão é pressionado, troca a saída e envia a requisição Bluetooth
-        pCharacteristic->setValue(""); // Limpa o valor da característica após ação
+        outputState[currentOutput] = !outputState[currentOutput];
+        digitalWrite(outputPins[currentOutput], outputState[currentOutput]);
+        int statusValue = outputState[currentOutput] ? 101 : 100;
+        String statusMessage = "porta " + String(outputPins[currentOutput]) + " " + (outputState[currentOutput] ? "ligada = " : "desligada = ") + String(statusValue);
+        pCharacteristic->setValue(statusMessage.c_str());
         pCharacteristic->notify();
+        currentOutput = (currentOutput + 1) % 3;
       }
     }
   }
